ee9ff23d30a1aebf6dd1fdb86b72d70bfb6176b6
estih923 <128614049+estih923@users.noreply.github.com>
2023-09-23 22:42:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 function generateAIPieces(aiTeamSize) {
     const aiTeam = [];
     const temperaments = ["Angry", "Greedy", "Devout", "Intellectual", "Speculative", "Confused"];
-    
+
     for (let i = 0; i < aiTeamSize; i++) {
         const pieceName = prompt(`Enter name for AI piece ${i + 1}:`);
         const temperament = temperaments[Math.floor(Math.random() * temperaments.length)];
         aiTeam.push({ name: pieceName, temperament: temperament });
     }
-    
+
     return aiTeam;
 }
 
@@ -18,17 +18,19 @@ function generateAIPieces(aiTeamSize) {
 function simulateAIBehavior(strategy, aiTeam) {
     // Add your AI behavior simulation logic here
     // Consider strategy, temperament, and other game factors
+    // Return an object representing AI behavior
+    // Example: { piece1: "Move aggressively", piece2: "Defend", ... }
 }
 
-// Event listener for when the user selects a strategy and team size
-document.getElementById("strategy").addEventListener("change", function () {
-    const strategy = this.value;
+// Event listener for when the user clicks the "Generate AI" button
+document.getElementById("generate-ai").addEventListener("click", function () {
+    const strategy = document.getElementById("strategy").value;
     const aiTeamSize = parseInt(document.getElementById("ai-team-size").value, 10);
     const aiTeam = generateAIPieces(aiTeamSize);
-    
+
     // Simulate AI behavior based on strategy and aiTeam
     const aiBehavior = simulateAIBehavior(strategy, aiTeam);
-    
+
     // Display AI behavior on the web page
     const aiBehaviorElement = document.getElementById("ai-behavior");
     aiBehaviorElement.innerHTML = `<h2>AI Behavior:</h2><pre>${JSON.stringify(aiBehavior, null, 2)}</pre>`;
