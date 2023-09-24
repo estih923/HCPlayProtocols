78dfed7e92cbbff13e33f429f432f085b32e4c49
estih923 <128614049+estih923@users.noreply.github.com>
2023-09-24 19:03:23 -0400

    update

--- script.js
+++ script.js
@@ -1,39 +1,76 @@
-// Function to toggle the selected state for strategy buttons
-function toggleStrategyButton(buttonId) {
-    const buttons = document.querySelectorAll(".strategy-btn");
-    buttons.forEach((button) => {
-        button.classList.remove("selected");
+document.addEventListener("DOMContentLoaded", function () {
+    const teamTacticSelect = document.getElementById("team-tactic");
+    const selectTacticBtn = document.getElementById("select-tactic-btn");
+    const tacticDetails = document.getElementById("tactic-details");
+    const tacticDescription = document.getElementById("tactic-description");
+    const opponentCountInput = document.getElementById("opponent-count");
+    const nameOpponentsBtn = document.getElementById("name-opponents-btn");
+    const opponentsContainer = document.getElementById("opponents");
+    const resetBtn = document.getElementById("reset-btn");
+
+    // Define team tactics and temperament parameters
+    const teamTactics = {
+        offensive: {
+            description: "The AI prioritizes targeting a single opponent's character at a time, concentrating its attacks to eliminate key threats quickly and reduce the enemy's firepower."
+        },
+        balanced: {
+            description: "The AI analyzes the opponent's strategy and adapts its tactics accordingly, shifting between offensive and defensive strategies as the situation demands."
+        },
+        defensive: {
+            description: "The AI selects a strong defensive position on the map and hunkers down, using characters with Barrier and Support powers to maintain a protective perimeter."
+        }
+    };
+
+    const temperamentParameters = {
+        angry: "Highly aggressive and prioritizes attacking enemies whenever possible.",
+        greedy: "Opportunistic and attacks when there's a chance for significant gain.",
+        devout: "Follows a balanced approach, considering both offense and defense.",
+        intellectual: "Cautious and prioritizes defense over offense.",
+        speculative: "Highly defensive and risk-averse.",
+        confused: "Exhibits unpredictable behavior, sometimes attacking and sometimes defending."
+    };
+
+    selectTacticBtn.addEventListener("click", function () {
+        const selectedTactic = teamTacticSelect.value;
+        const tacticInfo = teamTactics[selectedTactic].description;
+        tacticDescription.textContent = tacticInfo;
+        tacticDetails.classList.remove("hidden");
+        nameOpponentsBtn.classList.remove("hidden");
     });
-    document.getElementById(buttonId).classList.add("selected");
-}
 
-// Event listeners for strategy buttons
-document.getElementById("offensive-btn").addEventListener("click", function () {
-    // Handle offensive strategy selection
-    toggleStrategyButton("offensive-btn");
-});
+    nameOpponentsBtn.addEventListener("click", function () {
+        const opponentCount = parseInt(opponentCountInput.value);
+        createOpponents(opponentCount);
+        opponentsContainer.classList.remove("hidden");
+        resetBtn.classList.remove("hidden");
+    });
 
-document.getElementById("balanced-btn").addEventListener("click", function () {
-    // Handle balanced strategy selection
-    toggleStrategyButton("balanced-btn");
-});
+    resetBtn.addEventListener("click", function () {
+        teamTacticSelect.value = "";
+        tacticDetails.classList.add("hidden");
+        nameOpponentsBtn.classList.add("hidden");
+        opponentsContainer.innerHTML = ""; // Clear opponents list
+        resetBtn.classList.add("hidden");
+    });
 
-document.getElementById("defensive-btn").addEventListener("click", function () {
-    // Handle defensive strategy selection
-    toggleStrategyButton("defensive-btn");
-});
+    function createOpponents(count) {
+        const opponentTemperament = selectRandomTemperament();
+        const opponentList = document.createElement("ul");
 
-// Event listener for the "Name Your Pieces" button
-document.getElementById("name-pieces-btn").addEventListener("click", function () {
-    const aiTeamSize = parseInt(document.getElementById("num-ai-pieces").value, 10);
-    const pieceNames = [];
+        for (let i = 1; i <= count; i++) {
+            const opponentName = prompt(`Enter a name for opponent ${i}:`);
+            const opponent = document.createElement("li");
+            opponent.textContent = `${opponentName} (Temperament: ${opponentTemperament})`;
+            opponentList.appendChild(opponent);
+        }
 
-    for (let i = 0; i < aiTeamSize; i++) {
-        const pieceName = prompt(`Enter name for AI piece ${i + 1}:`);
-        pieceNames.push(pieceName);
+        opponentsContainer.innerHTML = ""; // Clear previous opponents
+        opponentsContainer.appendChild(opponentList);
     }
 
-    // Display the generated piece names
-    const pieceNamesElement = document.getElementById("piece-names");
-    pieceNamesElement.innerHTML = `<h3>AI Piece Names:</h3>${pieceNames.join("<br>")}`;
+    function selectRandomTemperament() {
+        const temperaments = Object.keys(temperamentParameters);
+        const randomIndex = Math.floor(Math.random() * temperaments.length);
+        return temperaments[randomIndex];
+    }
 });
