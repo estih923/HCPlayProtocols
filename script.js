b55d51f2349496cee2874f6c106b1702029aa48a
estih923 <128614049+estih923@users.noreply.github.com>
2023-09-24 13:10:12 -0400

    Update to site

--- script.js
+++ script.js
@@ -1,48 +1,39 @@
-// JavaScript logic
+// Function to toggle the selected state for strategy buttons
+function toggleStrategyButton(buttonId) {
+    const buttons = document.querySelectorAll(".strategy-btn");
+    buttons.forEach((button) => {
+        button.classList.remove("selected");
+    });
+    document.getElementById(buttonId).classList.add("selected");
+}
 
-// Function to generate AI team pieces with temperaments
-function generateAIPieces(aiTeamSize) {
-    const aiTeam = [];
-    const temperaments = ["Angry", "Greedy", "Devout", "Intellectual", "Speculative", "Confused"];
+// Event listeners for strategy buttons
+document.getElementById("offensive-btn").addEventListener("click", function () {
+    // Handle offensive strategy selection
+    toggleStrategyButton("offensive-btn");
+});
+
+document.getElementById("balanced-btn").addEventListener("click", function () {
+    // Handle balanced strategy selection
+    toggleStrategyButton("balanced-btn");
+});
+
+document.getElementById("defensive-btn").addEventListener("click", function () {
+    // Handle defensive strategy selection
+    toggleStrategyButton("defensive-btn");
+});
+
+// Event listener for the "Name Your Pieces" button
+document.getElementById("name-pieces-btn").addEventListener("click", function () {
+    const aiTeamSize = parseInt(document.getElementById("num-ai-pieces").value, 10);
+    const pieceNames = [];
 
     for (let i = 0; i < aiTeamSize; i++) {
         const pieceName = prompt(`Enter name for AI piece ${i + 1}:`);
-        const temperament = temperaments[Math.floor(Math.random() * temperaments.length)];
-        aiTeam.push({ name: pieceName, temperament: temperament });
+        pieceNames.push(pieceName);
     }
 
-    return aiTeam;
-}
-
-// Function to simulate AI behavior based on strategy and temperament
-function simulateAIBehavior(strategy, aiTeam) {
-    const aiBehavior = {};
-
-    // Example simulation logic - replace with your own
-    aiTeam.forEach((piece, index) => {
-        if (strategy === "offensive") {
-            aiBehavior[`piece${index + 1}`] = `${piece.name} - Move aggressively`;
-        } else if (strategy === "defensive") {
-            aiBehavior[`piece${index + 1}`] = `${piece.name} - Defend`;
-        } else if (strategy === "balanced") {
-            aiBehavior[`piece${index + 1}`] = `${piece.name} - Take balanced action`;
-        }
-    });
-
-    return aiBehavior;
-}
-
-// Event listener for when the user clicks the "Generate AI Team" button
-document.getElementById("generate-ai-team").addEventListener("click", function () {
-    const strategy = document.getElementById("strategy-select").value;
-    const aiTeamSize = parseInt(document.getElementById("num-ai-pieces").value, 10);
-    const aiTeam = generateAIPieces(aiTeamSize);
-
-    // Simulate AI behavior based on strategy and aiTeam
-    const aiBehavior = simulateAIBehavior(strategy, aiTeam);
-
-    // Display AI behavior on the web page
-    const aiBehaviorElement = document.getElementById("ai-behavior");
-    aiBehaviorElement.innerHTML = `<h2>AI Behavior:</h2><pre>${JSON.stringify(aiBehavior, null, 2)}</pre>`;
+    // Display the generated piece names
+    const pieceNamesElement = document.getElementById("piece-names");
+    pieceNamesElement.innerHTML = `<h3>AI Piece Names:</h3>${pieceNames.join("<br>")}`;
 });
-
